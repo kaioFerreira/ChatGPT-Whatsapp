2a24fb2d5a5e943d72bbca43bb33c9b2d5df9bb9
kaioFerreira <kaiofhs@hotmail.com>
2023-04-02 20:47:09 -0300

    teste request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { http } = require("./http");
 require("dotenv").config();
 
-http.listen(3333, () => {
-    console.log("Server Started On Port 3333");
+http.listen(process.env.PORT || 3333, () => {
+    console.log("Server Started");
 });
